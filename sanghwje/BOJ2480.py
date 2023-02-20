895bad18c653593672d0f420ae135cb212536ae6
sanghwa <sanghwaj222@gmail.com>
2023-02-20 21:23:39 +0900

    파이썬 백준문제풀이2

---
+++ sanghwje/BOJ2480.py
@@ -0,0 +1,11 @@
+a,b,c = map(int, input().split())
+if a == b == c:
+    print(10000+1000*a)
+elif a == b:
+    print(1000 + 100*a)
+elif a == c:
+    print(1000 + 100*a)
+elif b == c:
+    print(1000 + 100*b)
+else:
+    print(max(a,b,c)*100)
