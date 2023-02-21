8691507df0494cf0b5e353e0d9cdedb24cc23ec9
홍광표 <pyo@hong-gwangpyoui-MacBookAir.local>
2023-02-22 01:15:35 +0900

    a

---
+++ pyo/Development/i am Rich/I Am Poor.swift
@@ -0,0 +1,8 @@
+//
+//  I Am Poor.swift
+//  i am Rich
+//
+//  Created by 홍광표 on 2023/02/20.
+//
+
+import Foundation
---
+++ pyo/Development/i am Rich/i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by 홍광표 on 2023/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
