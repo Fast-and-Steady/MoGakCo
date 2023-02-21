8f6d7328457adb48d1935ea51fbcdfef5a685524
sanghwa <sanghwaj222@gmail.com>
2023-02-22 02:01:10 +0900

    0222백준문제풀이

---
+++ sanghwje/BOJ2292.py
@@ -0,0 +1,13 @@
+a = int(input())
+i = 0
+c = 0
+while 1:
+    if a == 1:
+        print(1)
+        break
+    elif a <= 6 * c + 1:
+        print(i)
+        break
+    c = c + i
+    i = i + 1
+# 수학내용이라 숙지할 것은 없고 뇌지컬을 기르는 문제였당.
