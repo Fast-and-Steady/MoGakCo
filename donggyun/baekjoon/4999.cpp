9aa6a53b09d1f4151bd810628cd79ff4eadd4ca8
서동균 <>
2023-03-17 00:11:22 +0900

    출석체크

---
+++ donggyun/baekjoon/4999.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string str1;
+	string str2;
+	cin >> str1;
+	cin >> str2;
+	if (str1 > str2) cout << "no";
+	else cout << "go";
+	return (0);
+}
