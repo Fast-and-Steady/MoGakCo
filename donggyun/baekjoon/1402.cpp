e1ebfd4506004d3515e21d64ef521f4e04672fb4
서동균 <>
2023-03-17 14:51:46 +0900

    백준문제풀이

---
+++ donggyun/baekjoon/1402.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, n;
+	cin >> n;
+	for (int x = 0; x < n; x++)
+	{
+		cin >> a >> b;
+		cout << "yes\n";
+	}
+    return 0;
+}
