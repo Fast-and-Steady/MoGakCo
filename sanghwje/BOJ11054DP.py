8fa09618fb4c2a4533e307be73243da116f8acac
sanghwa <sanghwaj222@gmail.com>
2023-06-09 00:08:32 +0900

    0608DP Gold4-solve

--- sanghwje/BOJ11054DP.py
+++ sanghwje/BOJ11054DP.py
@@ -1,14 +1,3 @@
-N = int(input())
-A = list(map(int, input().split()))
-Ldp = [[1 for i in range(N)] for j in range(N)]
-Rdp = [[1 for i in range(N)] for j in range(N)]
-
-for i in range(N):
-    j = i - 1
-    while j >= 0:
-        if A[i] > A[j]:
-            Ldp[i][j] += Ldp[j]
-        j -= 1
     Ldp[i] = max(Ldp[i])
 i = N-1
 while i >= 0:
@@ -28,6 +17,6 @@ for i in range(N):
     Ldp[i] += Rdp[i]
     if Ldp[i] + Rdp[i] == 0:
         Ldp[i] = 1
-    
+
 print(max(Ldp) - 1)
-    
+ 
